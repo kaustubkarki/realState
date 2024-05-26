05024ca28bf5e89802e89c2672f660f3feddf6fe
Kaustub karki <kaustubkarki@gmail.com>
2024-05-26 22:37:00 +0545

    landing Page updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import "./layout.scss";
 import Navbar from "./components/navbar/Navbar";
+import HomePage from "./routes/home/HomePage";
 
 function App() {
   return (
     <div className="layout">
       <Navbar />
+      <HomePage />
     </div>
   );
 }
 
 export default App;
-//36:36
