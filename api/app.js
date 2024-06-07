74ce4d088206414bb8728ab9ffb3fa5101c97f9d
Kaustub karki <kaustubkarki@gmail.com>
2024-06-07 21:40:38 +0545

    auth completed

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 
 const app = express();
 const port = 5000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser())
 
 // creating api end point
 app.use("/api/v1/auth", authRoute);
@@ -12,5 +14,3 @@ app.use("/api/v1/auth", authRoute);
 app.listen(port, () => {
   console.log(`server is running on port: ${port}`);
 });
-
-                           
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcrypt, { hashSync } from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
+
 import { PrismaClient, Prisma } from "@prisma/client";
 
 export const register = async (req, res) => {
@@ -38,23 +40,71 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
   const { username, password } = req.body;
+
   try {
     //! CHECK IS THE USER EXIST
-    //! CHECK IF THE PASSWORD IS CORRECT
-    //! GENERATE COOKIE TOKEN AND SEND TO THE USER
-  } catch (error) {}
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
 
-  res.status(200).json({
-    success: true,
-    message: "login page",
-  });
+    if (!user)
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+
+    //! CHECK IF THE PASSWORD IS CORRECT
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    // res.setHeader("set-Cookie", "test=" + "my val").json({
+    //   success: true,
+    //   message: "Login vayo",
+    // });
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY
+    );
+    res
+      .cookie("token", token, {
+        // httpOnly: true,
+        maxAge: 1000 * 60,
+        // secure:true, as localhost is only http we cant use this
+      })
+      .json({
+        success: true,
+        message: "cookie is set",
+      });
+    //! GENERATE COOKIE TOKEN AND SEND TO THE USER
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const logout = (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "logout page",
-  });
+  try {
+    res
+      .status(201)
+      .cookie("token", "", {
+        expires: new Date(0),
+        path: "/",
+        // sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+        // secure: process.env.NODE_ENV === "Development" ? false : true,
+      })
+      .json({
+        success: true,
+        message: "logout sucess",
+      });
+  } catch (error) {
+    next(error);
+  }
 };
