844fd568762a98c4da043ae737395b920dbe68ed
Kaustub karki <kaustubkarki@gmail.com>
2024-08-31 09:39:16 +0545

    chat and messages

--- api/app.js
+++ api/app.js
@@ -5,6 +5,8 @@ import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 
 const app = express();
 const port = 5000;
@@ -24,6 +26,8 @@ app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/posts", postRoute);
 app.use("/api/v1/test", testRoute);
 app.use("/api/v1/users", userRoute);
+app.use("/api/v1/chats", chatRoute);
+app.use("/api/v1/messages", messageRoute);
 
 app.listen(port, () => {
   console.log(
